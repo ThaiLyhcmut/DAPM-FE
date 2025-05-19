6e35544bc23ca8d8356544da04571d4954fd6cc3
thailyhcmut <lyvinhthai321@gmail.com>
2025-05-19 16:03:08 +0700

    update

--- store.ts
+++ store.ts
@@ -1,20 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import thunk from 'redux-thunk';
+import thucnkMiddleware from 'redux-thunk';  // Đổi tên biến cho rõ ràng
 import { rootReducer } from './reducers';
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['auth'] // Chỉ persist auth state
+  whitelist: ['auth'] 
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(
-  persistedReducer,
-  applyMiddleware(thunk.withExtraArgument({}))
-);
+
+export const store = createStore(persistedReducer);
+
+
+
 
 export const persistor = persistStore(store);
